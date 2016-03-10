af468e51c13c275da6a83fac9336a5a0914e4482
Himanshu <himanshu@Himanshus-MacBook-Pro.local>
2016-03-10 16:30:30 +0530

    temp

--- app/src/main/java/sample/carpool/delvelogic/poolmyridesample/Activity/MainActivity.java
+++ app/src/main/java/sample/carpool/delvelogic/poolmyridesample/Activity/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        CarpoolRequest.init(this);
+        CarpoolRequest.init(this);
         mainListView = (ListView) findViewById(R.id.mainListView);
         String[] fetchCarpool = getResources().getStringArray(R.array.fetch_carpool);
 
